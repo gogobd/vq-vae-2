083a9620317de5414199da5a89c4aee00df35550
Georg Gogo. BERNHARD <gogo@bluedynamics.com>
2020-05-16 21:35:21 +0000

    Trying to add tiled dataset for training

--- vq_vae_2/examples/hierarchical/data.py
+++ vq_vae_2/examples/hierarchical/data.py
@@ -14,6 +14,51 @@ import torch
 IMAGE_SIZE = 256
 
 
+class SwipeCropper(object):
+    
+    def __init__(self, image, wr, hr):
+        self.image = image
+        self.wr = wr
+        self.hr = hr
+        
+    def __iter__(self):
+        wr = self.wr
+        hr = self.hr
+        c, h, w = image.shape
+        if (h < hr) or (w < wr):
+            print("Image too small.")
+            return
+        hd = (hr - (h % hr)) / ( h // hr )
+        wd = (wr - (w % wr)) / ( w // wr )
+        for hn in range(h//hr + 1):
+            for wn in range(w//wr + 1):
+                h0 = int((hn * hr) - (hn * hd))
+                w0 = int((wn * wr) - (wn * wd))
+                h1 = int(h0 + hr)
+                w1 = int(w0 + wr)
+                yield(image[:, h0:h1, w0:w1])
+
+                
+def load_tiled_images(dir_path, width, height):
+    while True:
+        with os.scandir(dir_path) as listing:
+            for entry in listing:
+                if not (entry.name.endswith('.png') or entry.name.endswith('.jpg')):
+                    continue
+                try:
+                    img = Image.open(entry.path)
+                except OSError:
+                    # Ignore corrupt images.
+                    continue
+                cropper = SwipeCropper(img, width, height)
+                while true:
+                    cropped_image = next(cropper)
+                    if cropped_image:
+                        yield cropped_image
+                    else:
+                        continue
+
+                
 def load_images(dir_path, batch_size=16):
     images = load_single_images(dir_path)
     while True:
